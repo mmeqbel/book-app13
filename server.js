a3c34969a302160303f3975cd63aa025a48727a0
motasemAlsqoor <motasem900khaled@gmail.com>
2021-03-02 13:57:15 +0200

    code refactor

--- server.js
+++ server.js
@@ -1,116 +1,108 @@
+/***********************************************************************/
+/************************IMPORT MODULES*********************************/
+/***********************************************************************/
+
 // load the server dependencies
 const express = require('express');
 const cors = require('cors');
 const ejs = require('ejs');
-const pg = require('pg');
-// configuration
-require('dotenv').config();
-const app = express();
-const superagent = require('superagent');
-const { text } = require('express');
-app.use(cors());
-const client = new pg.Client(process.env.DATABASE_URL);
-//const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });//heroko
+const pg = require('pg');//postgress module
+const dotenv = require('dotenv');
+const superagent = require('superagent');//super agent module help in working with api's
 
-app.use(express.static('./public'));
-app.use(express.urlencoded({ extended: true }));
+/***********************************************************************/
+/**********************SERVER CONFIGURATION******** ********************/
+/***********************************************************************/
+
+dotenv.config(); //configure .env file 
 
 const PORT = process.env.PORT;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+const client = new pg.Client(process.env.DATABASE_URL);     //configure  client db
+//const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });//heroko
+const app = express();
+app.use(cors());
+app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');      // set the view engine to ejs
 
-// use res.render to load up an ejs view file
 
-/********************************** **
-***************END POINTS  ***********
-***************************************/
-// index page 
-app.get('/hello', (req, res) => {
-    console.log("the main route")
-    res.render('./pages/index');
-});
+
+/***********************************************************************/
+/***********************END POINTS**************************************/
+/***********************************************************************/
+
+/******HOME ******/
+app.get('/', (req, res) => {
+    const query = 'SELECT author,title,isbn,image_url,description FROM books';
+    client.
+        query(query).
+        then(data => {
+            res.render('./pages/index', { "books": data.rows })
+        }).catch(error => {
+            console.log(error);
+            res.render('./pages/error', { "error": error })
+        });
+})
+
+/******SEARCH ******/
 app.get('/searches/new', (req, res) => {
     // console.log(res, req);
     res.render('./pages/searches/new')
 });
-app.get('/', (req, res) => {
-    const query='SELECT author,title,isbn,image_url,description FROM books';
-    client.
-    query(query).
-    then(data=>{
-        res.render('./pages/index',{"books":data.rows})
-    }).catch(error=>{
-        console.log(error);
-        res.render('./pages/error', { "error": error })
-    });
-    
-})
-app.post('/searches/show', (req, res) => {
-    //{ searchQuery: 'hello', searchBy: 'title', search: 'search' }
-    //request google  google book api 
 
+/******SEARCH RESULT ******/
+app.post('/searches/show', (req, res) => {
+    //request google  google book api 
     let baseAPIUrl = "https://www.googleapis.com/books/v1/volumes"
     let searchQuery = req.body.searchQuery + "+" + req.body.searchBy;
-    let query = {
-        q: searchQuery,
-    }
-    superagent.get(baseAPIUrl).query(query).then(data => {
-        // console.log(data.body.items);
-        var results = [];
-        for (let index = 0; index < 10; index++) {
-            const element = data.body.items[index];
-            const book = new Book(
-                element.volumeInfo.title,
-                element.volumeInfo.imageLinks,
-                element.volumeInfo.authors,
-                element.volumeInfo.description,
-                element.volumeInfo.industryIdentifiers
-            );
-            results.push(book);
-        }
-        res.render('./pages/searches/show', { "results": results });
-    }).catch(error => {
-        console.log(error);
-        res.render('./pages/error', { "error": error })
-    });
+    superagent
+        .get(baseAPIUrl)
+        .query({q: searchQuery})
+        .then(data => {
+            res.render('./pages/searches/show', { "results": getBookArray(data) });
+        })
+        .catch(error => {
+            res.render('./pages/error', { "error": error })
+        });
 });
-app.get('/books/:id', (req,res)=>{
+
+/******BOOK SHOW******/
+app.get('/books/:id', (req, res) => {
     let id = req.params.id;
-    const query='SELECT author,title,isbn,image_url,description FROM books WHERE id=$1';
+    const query = 'SELECT author,title,isbn,image_url,description FROM books WHERE id=$1';
     let safeValue = [id];
     client.
-    query(query,safeValue).
-    then(data=>{
-        res.render('./pages/books/show.ejs',{"books":data.rows})
-    }).catch(error=>{
-        console.log(error);
-        res.render('./pages/error', { "error": error })
-    }); 
-})
-app.get('/books/show', (req,res)=>{
-//     console.log("params =", req.params);
-// console.log("query is ",req.query);
-})
-app.post('/books', (req,res)=>{
-    const item=JSON.parse(req.body.item);
-    const dbQuery='INSERT INTO books (author, title, isbn, image_url,description)VALUES($1,$2,$3,$4,$5)'
-    const safeValues=[item.author,item.title,item.isbn,item.image_url,item.description]
-    console.log(safeValues)
-    client.
-    query(dbQuery,safeValues).then(data=>{
-        console.log("successfully inserted ");
-        console.log(data);
-        res.render('./pages/books/show.ejs',{"books":Array.of(item)})
-    }).catch(error=>{
-        console.log("error in inserting to db\n",error);
-        res.render('./pages/error', { "error": error })
-    })
+        query(query, safeValue).
+        then(data => {
+            res.render('./pages/books/show.ejs', { "books": data.rows })
+        }).catch(error => {
+            console.log(error);
+            res.render('./pages/error', { "error": error })
+        });
 })
 
-/********************************** **
-***************DATA MODEL  ***********
-***************************************/
+/******BOOK POST ******/
+app.post('/books', (req, res) => {
+    const item = JSON.parse(req.body.item);
+    const dbQuery = 'INSERT INTO books (author, title, isbn, image_url,description)VALUES($1,$2,$3,$4,$5)';
+    const safeValues = [item.author, item.title, item.isbn, item.image_url, item.description];
+    client
+        .query(dbQuery, safeValues).then(data => {
+            res.render('./pages/books/show.ejs', { "books": Array.of(item) })
+        })
+        .catch(error => {
+            res.render('./pages/error', { "error": error })
+        });
+});
+
+app.get('/hello', (req, res) => {
+    res.render('./pages/index');
+});
+
+/***********************************************************************/
+/***********************DATA MODEL**************************************/
+/***********************************************************************/
 function Book(title, img, authorName, description, isbn) {
     this.title = title || 'unknown title';
     this.image_url = secure(img) || 'https://i.imgur.com/J5LVHEL.jpg';
@@ -120,13 +112,12 @@ function Book(title, img, authorName, description, isbn) {
     this.isbn = formatIsbn(isbn) || "unavailable isbn";
 }
 
-/***************************************** 
-*****************************************
-***************helper********************
-*******************************************/
+/***********************************************************************/
+/***********************HELPER FUNCTIONS********************************/
+/***********************************************************************/
 function secure(img) {
-    if(typeof img==typeof undefined) return null;
-    let url= img.thumbnail;
+    if (typeof img == typeof undefined) return null;
+    let url = img.thumbnail;
     if (url[5] != 's') {
         var i = url.split("")
         i.splice(4, 0, 's');
@@ -134,21 +125,38 @@ function secure(img) {
     return i.join("");
 }
 function formatIsbn(isbn) {
-    if (isbn.length!=0) 
+    if (isbn.length != 0)
         return isbn[0].type + " " + isbn[0].identifier//isbn
     return null;
 }
-function formatAuthor(author){
-    if(typeof author==typeof undefined) return null;
-    if (author.length!=0) 
+function formatAuthor(author) {
+    if (typeof author == typeof undefined) return null;
+    if (author.length != 0)
         return author.join(", ");
     return null;
 }
-client.connect().then(()=>{
+function getBookArray(data) {
+    const temp = [];
+    for (let index = 0; index < 10; index++) {
+        const element = data.body.items[index];
+        const book = new Book(
+            element.volumeInfo.title,
+            element.volumeInfo.imageLinks,
+            element.volumeInfo.authors,
+            element.volumeInfo.description,
+            element.volumeInfo.industryIdentifiers
+        );
+        temp.push(book);
+    }
+    return temp;
+}
+/***********************************************************************/
+/*************************RUN*******************************************/
+/***********************************************************************/
+client.connect().then(() => {
     app.listen(PORT, () => {
         console.log('app is lestining in port ....', PORT);
     });
-}).catch(error=>{
+}).catch(error => {
     console.log('error app is not lestining in port ....', error);
 });
-
